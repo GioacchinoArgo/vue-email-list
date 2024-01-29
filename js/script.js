566cb5e2b1a4650fab8443bf0460e6a5ff435f7f
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-29 17:56:40 +0100

    completed exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log('Vue OK', Vue);
+
+const {createApp} = Vue;
+
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
+const app = createApp({
+    data: () =>  ({
+        mails: []
+    }),
+    created() {
+        for (let i = 0; i < 10; i++) 
+        axios.get(endpoint).then((res) => {
+            this.mails.push(res.data.response)
+        })
+    }
+});
+
+app.mount('#root')
